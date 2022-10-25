5e7e4fe9901f596b16e0bb16ffd003deb704d044
Iam Estrella <ikestrella@utpl.edu.ec>
2022-10-25 00:24:28 -0500

    claseprogramacion

---
+++ Git.java
@@ -0,0 +1,9 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("git init: Con este comando de git se crea un nuevo repositorio o solo para iniciar un repositorio vacio");
+        System.out.println("git add: Mediante este comando se pasan archivos cambiados, o cualquier tipo de archivo");
+        System.out.println("git commit -m \"first commit\": Con este comando se confirma de manera instantenea los archivos que queremos pasar al repositorio");
+        System.out.println("git branch -M main: Se crea una linea de desarrollo principal, y adicionalmente se pueden añadir ramas para asi poder trabajar en otra funcion ");
+        System.out.println("git push -u origin main: se usa este comando para cargar los archivos al repositorio");
+    }
+}
---
+++ src/Contenido.java
@@ -0,0 +1,29 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("20 Conceptos Basicos de la Programacion ");
+        System.out.println("");
+        System.out.println("- Paradigma de la Programacion: Es un tipo de desarrollo el cual sirve para desarrollar programas" +
+                "con la finalidad de resolver problemas");
+        System.out.println("- Lenguaje de Programacion: Los lenguajes de programacion se utilizan para comunicarse con la computadora" +
+                "y asi poder implementar un conjunto de instrucciones ");
+        System.out.println("- Algoritmo: Son conjuntos de instrucciones los cuales se los implementan para resolver problemas");
+        System.out.println("- Programa Informatico: Secuencia de instrucciones en orden que sirven para desarrollar un programa");
+        System.out.println("- Software de aplicacion: son programas secundarios los cuales permiten desarrollar funciones como word, excel, etc");
+        System.out.println("- Software de sistema: Son programas los cuales permiten el uso de diferentes tipos de hardware, como sistemas operativos, controladores de hardware, etc");
+        System.out.println("- PSeInt: PSeInt es una herramienta en la cual se escriben algoritmos en pseudocódigos.");
+        System.out.println("- Pseudocodigos: Son secuencias de instrucciones que se dan para resolver problemas.");
+        System.out.println("- Compilacion: Programa utilizado para traducir el lenguaje de programacion al lenguaje de computadora- ");
+        System.out.println("- Depuracion: proceso mediante el cual se realiza una buscan errores de programacion que puedeb haber en el programa");
+        System.out.println("- Identificadores: sirven para identificar elementos dentro del programa mediante simbolos, o palabras");
+        System.out.println("- Palabras reservadas: conjunto de expresiones de la cual esta formado un lenguaje de programacion");
+        System.out.println("- Variables: Espacio donde se almacenan y mediante el cual se puede recuperar informacion");
+        System.out.println("- Constantes: Variable que no se puede cambiar en el programa");
+        System.out.println("- Big Data: Utiliza datos almacenados puedan ser utilizados");
+        System.out.println("- Operadores de datos: Elemento de la aplicacion que se encarga de aplicar a uno o varios operandos");
+        System.out.println("- Estructuras de control: Permiten modificar el flujo de ejecucion de un programa");
+        System.out.println("- Codigo Fuente: los codigos fuentes son escritos con un lenguaje de programacion de forma de texto plano");
+        System.out.println("- Sintaxis: Lenguaje de programacion el cual esta definido por un conjunto de normas que se deben seguir en orden para que no hayan errores con el lenguaje de programacion");
+        System.out.println("- Lenguaje de programacion: Herramienta mediante la cual se desarrollan programas para dispositivos");
+        System.out.println("- Codigo: cumulo de normas que se deben seguir para que un programa pueda ser utilizado");
+    }
+}
