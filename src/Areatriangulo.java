7ce6db66667631b1335e455e34fa0ec694231332
ikestrella <ikestrella@utpl.edu.ec>
2022-10-30 12:51:24 -0500

    trabajo

--- src/Areatriangulo.java
+++ src/Areatriangulo.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Areatriangulo {
+import java.util.Scanner;
+
+public class Areatriangulo {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("PROGRAMA PARA CALCULAR EL AREA DE UN TRIANGULO");
+        System.out.println("Ingrese el valor de la base: ");
+        double base = teclado.nextDouble();//se lee el valor dado por el teclado
+        System.out.println("Ingrese el valor de la altura: ");
+        double altura = teclado.nextDouble();//se lee el valor dado por el teclado
+        double area = (base*altura)/2;
+        double resultado = Math.round(area);
+        System.out.println("El area del triangulo es: " + area);
+    }
 }
--- src/Libreriamath.java
+++ src/Libreriamath.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Libreriamath {
+import java.util.Scanner;
+
+public class Libreriamath {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        double numero = 9;
+        //funcion para calcular la raiz cuadrada de un numero
+        System.out.println("Calculo de la raiz cuadrada: ");
+        double raiz = Math.sqrt(numero);
+        System.out.println("El resultado de la raiz es: " + raiz);
+        //funcion para calcular la potencia de un numero
+        System.out.println("Calculo de la potencia de un numero : ");
+        double exp = 2;
+        double potencia = Math.pow(numero, exp);
+        System.out.println("Resultado del numero elevado al cuadrado es: " + potencia);
+
+        int a=1;
+        int b=1;
+        double c=1;
+        c = Math.sqrt(Math.pow(a,2)+ Math.pow (b,2));
+        System.out.println("El valor de la hipotenusa es: "+ c);
+    }
 }
