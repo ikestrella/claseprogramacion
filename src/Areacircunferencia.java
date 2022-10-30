cf357c0eec021a0e999c56361718f532d180e115
ikestrella <ikestrella@utpl.edu.ec>
2022-10-30 12:44:26 -0500

    trabajo

---
+++ src/Areacircunferencia.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Areacircunferencia {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.println("Programa para obtener el área de una circunferencia");
+        System.out.println("Ingrese el radio de la circunferencia: ");
+        double radio = teclado.nextDouble();
+        double area = (Math.PI) * (radio*radio);
+        System.out.println("El area de la circunferencia es: "+ area);
+    }
+}
