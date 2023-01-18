4630221a01a2ae30578ea08d78edf3939041bb07
ikestrella <ikestrella@utpl.edu.ec>
2023-01-18 02:17:23 -0500

    Trabajos enviados

--- src/Borradores/Borrador4.java
+++ src/Borradores/Borrador4.java
@@ -1,18 +1,23 @@
-package Bimestre2_semana6;
+package Borradores;
+
 import java.util.Scanner;
 
-public class Ejercicio3 {
-    int cedula [][]=new int[5][10];
-    public void ingresar() {
-        Ejercicio3 asdd= new Ejercicio3();
+public class Borrador4 {
+    public void ingresar(){
+        int ced [][]=new int[5][10];
         Scanner sc = new Scanner(System.in);
-        for (int indice = 0; indice < cedula.length; indice++) {
+        for (int indice = 0; indice < ced.length; indice++) {
+            int indice2=0;
             System.out.println("Ingrese el numero de cedula");
-            for (int indice2 = 0; indice2 < cedula[0].length; indice2++) {
-                int ced = sc.nextInt();
-                cedula[indice][indice2] = ced;
+            for (indice2 = 0; indice2 < ced[0].length; indice2++) {
+                int numc = sc.nextInt();
+                ced[indice][indice2] = numc;
             }
         }
+        Borrador4 asd=new Borrador4();
+        asd.recibir(ced);
+    }
+    public void recibir(int[][] cedula){
         for (int indice = 0; indice < cedula.length; indice++) {
             for (int indice2 = 0; indice2 < cedula[0].length-1; indice2=indice2+2) {
                 cedula[indice][indice2]=cedula[indice][indice2]*2;
@@ -56,7 +61,7 @@ public class Ejercicio3 {
         }
     }
     public static void main(String[] args) {
-        Ejercicio3 asd= new Ejercicio3();
-        asd.ingresar();
+        Borrador4 scs= new Borrador4();
+        scs.ingresar();
     }
 }
